b4023c4b4d0923b98833cef5c0173f03c91248af
alstacon <alstacon@gmail.com>
2022-12-20 18:13:36 +0400

    admin model

--- core/admin.py
+++ core/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from core.models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class CustomAdminUser(admin.ModelAdmin):
+    readonly_fields = ('last_login', 'date_joined')
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    search_fields = ['username', 'email', 'first_name', 'last_name']
+    list_filter = ('is_staff', 'is_active', 'is_superuser')
+
+

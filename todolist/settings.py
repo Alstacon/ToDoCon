778b10b7838c9c19fddc34c084282f3845214ca3
alstacon <alstacon@gmail.com>
2023-01-11 10:41:56 +0400

    oauth

--- todolist/settings.py
+++ todolist/settings.py
@@ -43,6 +43,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'todolist.urls'
